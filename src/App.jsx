0161f9770fd49b367cdc651368d6ddf97486e09d
vadim <yurkinvadim99@gmail.com>
2025-09-27 13:22:10 +0200

    colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-function App() {
+import {ColorModeContext, useMode} from "./theme.js";
+import {CssBaseline, ThemeProvider} from "@mui/material";
 
-  return (
-    <>
-      <h1>Hi!</h1>
-    </>
-  )
+
+function App() {
+    const [theme, colorMode] = useMode();
+
+
+    return (
+        <ColorModeContext.Provider value={colorMode}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline/>
+                <div className={'app'}>
+                    <main className={'content'}>
+                        <h1>Hi!</h1>
+                    </main>
+                </div>
+            </ThemeProvider>
+        </ColorModeContext.Provider>
+    )
 }
 
 export default App
