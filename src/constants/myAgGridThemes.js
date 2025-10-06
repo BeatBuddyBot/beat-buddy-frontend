3a51bc132c4625a49491f6b7890a20388733567f
vadim <yurkinvadim99@gmail.com>
2025-10-07 01:09:24 +0200

    alt theme for table

--- src/constants/myAgGridThemes.js
+++ src/constants/myAgGridThemes.js
@@ -1,4 +1,5 @@
 import {themeQuartz} from "ag-grid-community";
+import {grey} from "@mui/material/colors";
 
 export const myDarkAgGridTheme = themeQuartz
     .withParams({
@@ -12,4 +13,25 @@ export const myDarkAgGridTheme = themeQuartz
         foregroundColor: "#FFF",
         headerBackgroundColor: "#0B0D0F",
         headerFontSize: 14
+    });
+
+
+export const myAltDarkAgGridTheme = themeQuartz
+    .withParams({
+        backgroundColor: "#0b0d0f",
+        browserColorScheme: "dark",
+        chromeBackgroundColor: {
+            ref: "foregroundColor",
+            mix: 0.07,
+            onto: "backgroundColor"
+        },
+        foregroundColor: "#FFF",
+        headerBackgroundColor: "#0B0D0F",
+        headerFontSize: 14,
+
+        borderColor: grey[500],
+        columnBorder: false,
+        headerRowBorder: false,
+        wrapperBorder: false
+
     });
